a66fc96ad83008de07b9a87b70c6a2342ec1edfe
dipoahmed018 <dipoahmed018@gmail.com>
2021-05-07 21:00:43 +0600

    trying to fighre out a way to implement friend relationships

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Message;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MessageFactory extends Factory
@@ -22,7 +23,25 @@ class MessageFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'content' => 'hello world',
+            'owner' => User::all()->random()->id,
         ];
+    }
+    public function toFriends()
+    {
+        return $this->state(function(array $attributes){
+            
+        });
+    }
+    public function toGroup()
+    {
+        return $this->state(function(array $attributes){
+            $groups = User::find(1)->groups;
+            $group = collect($groups)->random();
+            return [
+                'receiver_id' => $group->id,
+                'receiver_type' => 'group',
+            ]
+        });
     }
 }
