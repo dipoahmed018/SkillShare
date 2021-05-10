9f9a777fe5aac0bcc4d148e9e23169695f56040a
dipoahmed018 <dipoahmed018@gmail.com>
2021-05-11 05:37:59 +0600

    php artisan storage structure created

---
+++ database/migrations/2021_05_10_132739_create_file_link_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileLinkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_link', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('file_link')->nullable();
+            $table->enum('file_type',['tutorial','records','introduction','post','comment','message','profile_photo','profile_video']);
+            $table->enum('security',['public','private']);
+            $table->enum('fileable_type',['course','tuition','post','comment','message','profile']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_link');
+    }
+}
