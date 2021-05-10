3f1dc44087d1cac6518d6e1d2c30e8cbf20d6994
dipoahmed018 <dipoahmed018@gmail.com>
2021-05-11 05:57:00 +0600

    Request User update

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\LoginRequest;
+use App\Http\Requests\User\LogoutRequest;
+use App\Http\Requests\User\RegisterRequest;
+use App\Http\Requests\User\UpdateRequest;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function Register(RegisterRequest $inputs)
+    {
+        
+    }
+    public function Login(LoginRequest $inputs)
+    {
+        
+    }
+    public function Logout(LogoutRequest $inputs)
+    {
+        
+    }
+    public function Update(UpdateRequest $inputs)
+    {
+        
+    }
 }
